db273dd1d4c75f7007b417ba50c0de56fc1ca3a6
Goswin <gwinsky@yahoo.com>
2022-06-14 10:26:54 +0200

    add files

---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿let code = """
+try
+    8 / 0 |> ignore
+with e -> 
+    printfn $"Runtime: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}"
+    printfn $"Error: {e}" 
+"""
+
+open System
+open System.IO
+open FSharp.Compiler.Interactive.Shell
+
+let eval(codeStr) = 
+    let inn = new StringReader("")
+    let out = Console.Out
+    let config = FsiEvaluationSession.GetDefaultConfiguration()
+    let noArgs = [| "the first arg is neded, but ignored"|]
+    let session = FsiEvaluationSession.Create(config, noArgs, inn, out, out)
+    session.EvalInteraction(codeStr)
+
+eval(code)
+    
+System.Console.ReadLine() |> ignore
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ErrorLineNumberMissing
+
+This repository is to show that fsi used via FSharp.Compiler.Service 41.0.4 does not print the line numbers of exceptions anymore when used via net48.
+With net6.0 they show. 
